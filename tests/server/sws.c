1792df126dbfaebee21597afab960ccc51d34258
Viktor Szakats <commit@vsz.me>
2024-09-09 01:54:34 +0200

    portname/pidname, make them all static to avoid shadowing in tftpd.c

--- tests/server/sws.c
+++ tests/server/sws.c
@@ -1974,6 +1974,9 @@ static int service_connection(curl_socket_t msgsock, struct httprequest *req,
   return -1;
 }
 
+static const char *pidname = ".http.pid";
+static const char *portname = ".http.port";
+
 int main(int argc, char *argv[])
 {
   srvr_sockaddr_union_t me;
@@ -1986,8 +1989,6 @@ int main(int argc, char *argv[])
   const char *unix_socket = NULL;
   bool unlink_socket = false;
 #endif
-  const char *pidname = ".http.pid";
-  const char *portname = ".http.port";
   struct httprequest *req = NULL;
   int rc = 0;
   int error;
